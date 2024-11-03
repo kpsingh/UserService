176bedf93363d4b06c599e2bb73d9742279cf2da
Krishna Pratap Singh <kpsingh1234@gmail.com>
2024-11-03 10:39:47 +0530

    token changes

--- src/main/java/com/lld4/userservice/models/Token.java
+++ src/main/java/com/lld4/userservice/models/Token.java
@@ -22,9 +22,9 @@ public class Token extends BaseModel {
         token.setUserid(user.getId());
         token.setValue("MyToken " + user.getHashedPassword());
 
-        Date date = new Date();
-        token.setIssuedDate(date); // token creation date
+        token.setIssuedDate(new Date()); // token creation date
 
+        Date date = new Date();
         date.setTime(date.getTime() + 30 + 24 * 60 * 60 * 1000);
         token.setExpiryDate(date); // expiry date 30 days after creation
 
