edb910df4fd84e8d4cb8a9fcaf5059d05baa2d8d
Krishna Pratap Singh <kpsingh1234@gmail.com>
2024-11-12 11:50:29 +0530

    Added the repository for Authorization, Client and AuthorizationConsent

--- src/main/java/com/lld4/userservice/security/models/AuthorizationConsent.java
+++ src/main/java/com/lld4/userservice/security/models/AuthorizationConsent.java
@@ -3,11 +3,7 @@ package com.lld4.userservice.security.models;
 import java.io.Serializable;
 import java.util.Objects;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
---
+++ src/main/java/com/lld4/userservice/security/repository/AuthorizationConsentRepository.java
@@ -0,0 +1,12 @@
+package com.lld4.userservice.security.repository;
+import java.util.Optional;
+
+import com.lld4.userservice.security.models.AuthorizationConsent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorizationConsentRepository extends JpaRepository<AuthorizationConsent, AuthorizationConsent.AuthorizationConsentId> {
+    Optional<AuthorizationConsent> findByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+    void deleteByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+}
